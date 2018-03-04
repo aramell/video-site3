797d33ab71cc970e2253fb4790ebc81553c2849d
Andrew <andrew.ramell@gmail.com>
2018-03-03 21:37:27 -0500

    basic contact form

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -2,12 +2,31 @@
 import React from 'react'
 //
 
-const Contact = () => (
-  <div className="contact">
-    <div className="message">
-      Have a question, comment or would like to inquire about our services - Please reach out! We'd love to hear from you
-    </div>
-  </div>
-)
+
+class Contact extends React.Component {
+    constructor(){
+      super()
+
+     const onChange = () => {
+
+      }
+    }
+    render(){
+      return(
+        <div className="contact">
+        <div className="message">
+          Have a question, comment or would like to inquire about our services - Please reach out! We'd love to hear from you
+          <br/>
+         name: <input /><br/>
+         email:<input /><br/>
+         message: <input type='textbox'/>
+          <button type='Submit' >Submit </button>
+        </div>
+      </div>
+    )
+      
+  }
+}
 
 export default Contact
+
