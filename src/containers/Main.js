e77bdbc9f561f38419d6415c8136cc72ab6f056f
Andrew <andrew.ramell@gmail.com>
2018-03-27 18:38:52 -0400

    created sub-main with video and button css grid

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,24 @@
 
 import React from 'react'
+import Video from './Video'
 
 
 
 const MainPage = () => (
   <div className="main-page" >
-    this is a test
+    <h3>We are a Central Pennsylvania based video services company specializing in custom videos tailored to your needs
+    </h3>
+    <div className="sub-main">
+      <div className="meet-team">
+        <button >About Us </button>
+      </div>
+      <div className="example-video" >
+        <Video />
+      </div>
+    </div>
+    <div className="footer">
+      This is the footer!
+    </div>
   </div>
 )
 
