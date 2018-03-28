14c705d7377f62c167b20d67a054ac0941cef680
Andrew <andrew.ramell@gmail.com>
2018-03-28 16:12:14 -0400

    sized background video

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,15 +5,15 @@ import Video from './Video'
 
 
 const MainPage = () => (
-  <div className="main-page" >
+  <div className="wrapper style1 special" id="one" >
     <h3>We are a Central Pennsylvania based video services company specializing in custom videos tailored to your needs
     </h3>
-    <div className="sub-main">
+    <div className="inner">
       <div className="meet-team">
           <a href="/about" class="btn"> Meet Us </a>
         </div>
       <div className="example-video" >
-        <Video />
+        {/* <Video /> */}
       </div>
     </div>
    
